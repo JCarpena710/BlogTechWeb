9b89a03fb649d80c71e0c78f9010d45d9eed0109
Jorge <jorge.carpena@tecsup.edu.pe>
2021-12-10 14:04:56 -0500

    commit4

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = "http://localhost:8080/api/";
 
-export const BASE_URL = "http://40f9-38-25-11-204.ngrok.io/api/";
+export const BASE_URL = "https://40f9-38-25-11-204.ngrok.io/api/";
